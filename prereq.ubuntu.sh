683e4fbd43bc328f454bcfb779b0880b2248d4a1
Moriyoshi Koizumi <moriyoshi.koizumi@open-c.jp>
2020-08-20 07:53:58 +0000

    It's always tiresome

--- prereq.ubuntu.sh
+++ prereq.ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SUDO=${SUDO:-sudo}
-${SUDO} apt install -qy curl libz-dev libxml2-dev libssl-dev make gcc g++ libtool automake autoconf pkg-config gperf meson nasm yasm
+${SUDO} apt install -qy curl libz-dev libxml2-dev libssl-dev make gcc g++ libtool automake autoconf cmake pkg-config gperf meson nasm yasm git
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -v
 ${HOME}/.cargo/bin/cargo install cargo-c
